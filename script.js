1d0112341392aed8e536549f8d7895eea3d7fdd7
Joram <joramrebolledo@gmail.com>
2023-08-07 00:26:42 +0800

    Add UI to the game and finished game loop

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 
-const btn = document.querySelector('button');
+const btns = document.querySelectorAll('button');
+const playerText = document.querySelector('.player');
+const cpuText = document.querySelector('.cpu');
+const scoreText = document.querySelector('.score');
+let playerScore = 0;
+let cpuScore = 0;
 
-btn.addEventListener('click', test);
-
-function test() {
-	const cpu = getComputerChoice();
-	//alert(gameCheck(userChoice(), cpu) + ' CPU: ' + cpu);
-	alert(playRound());
-}
+btns.forEach(function(btn) {
+	btn.addEventListener('click', gameStart);
+});
 
 function getComputerChoice() {
 	let choice = Math.floor(Math.random() * 3);
@@ -16,40 +17,51 @@ function getComputerChoice() {
 	else return 'scissor';
 }
 
-function userChoice() {
-	return prompt('rock? paper? scissor?');
+function gameStart(event) {
+	let choice = event.target.innerText.toLowerCase();
+	let compChoice = getComputerChoice();
+	console.log(choice);
+	playerText.innerText = `You: ${choice}`;
+	cpuText.innerText = `CPU: ${compChoice}`;
+	if (gameCheck(choice, compChoice) === 'You win') {
+		playerScore++;
+		playerText.innerText += ' WIN!'
+		cpuText.innerText += ' LOSE!'
+	}
+	else if (gameCheck(choice, compChoice) === 'You lose') {
+		cpuScore++;
+		playerText.innerText += ' LOSE!'
+		cpuText.innerText += ' WIN!'
+	}
+	else {
+		playerText.innerText += ' TIE!'
+		cpuText.innerText += ' TIE!'
+	}
+	scoreText.innerText = `You: ${playerScore} CPU: ${cpuScore}`;
+	if (playerScore >= 10 || cpuScore >= 10) {
+		if (playerScore > cpuScore) scoreText.innerText = 'YOU WIN!';
+		else scoreText.innerText = 'CPU WIN!';
+		playerScore = 0;
+		cpuScore = 0;
+	}
 }
 
 function gameCheck(userPick, compPick) {
-	if (userPick == 'rock' && compPick == 'paper') {
+	if (userPick === 'rock' && compPick === 'paper') {
 		return 'You lose';
 	}
-	else if (userPick == 'paper' && compPick == 'scissor') {
+	else if (userPick === 'paper' && compPick === 'scissor') {
 		return 'You lose';
 	}
-	else if (userPick == 'scissor' && compPick == 'rock') {
+	else if (userPick === 'scissor' && compPick === 'rock') {
 		return 'You lose';
 	}
-	else if (userPick == compPick) {
+	else if (userPick === compPick) {
 		return 'Tie'
 	}
 	else {
 		return 'You win';
 	}
-	//return userPick;
-	//return string win or lose
-}
-
-function playRound() {
-	let round = 5;
-	let score = 0;
-	for (let i = 0; i < round; i++) {
-		if ((gameCheck(userChoice(), getComputerChoice())) == 'You win') {
-			score++;
-		}
-	}
-	if (score > round / 2) return 'You win';
-	else return 'You lose';
 }
 
 //reusable
